49dceab14168a0e0a20be1ed65e01980ffcd8ebc
Br34th7aking <abhijit18231@iiitd.ac.in>
2025-08-23 17:03:52 +0530

    Add urls.py and views.py stub

--- backend/pingo_channels/views.py
+++ backend/pingo_channels/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from .models import Channel, Message
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ChannelListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, server_id):
+        pass
+
+    def post(self, request, server_id):
+        pass
+
+
+class ChannelDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, server_id, channel_id):
+        pass
+
+    def patch(self, request, server_id, channel_id):
+        pass
+
+    def delete(self, request, server_id, channel_id):
+        pass
+
+
+class MessageListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, server_id, channel_id):
+        pass
+
+    def post(self, request, server_id, channel_id):
+        pass
+
+
+class MessageDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, server_id, channel_id, message_id):
+        pass
+
+    def patch(self, request, server_id, channel_id, message_id):
+        pass
+
+    def delete(self, request, server_id, channel_id, message_id):
+        pass
